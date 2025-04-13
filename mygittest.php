ae14143b0e65b128b8116afd7d7cd4711e367553
Dubendra Yewale <dubendray@gmail.com>
2025-04-13 12:47:40 +0530

    conflict solved from pramod-pc

--- mygittest.php
+++ mygittest.php
@@ -1,9 +1,6 @@
 <?php
-<<<<<<< HEAD
 echo "Line added by dubendra pc";
-=======
 echo "Line added from pramod pc";
->>>>>>> bb7dac2998f31702556e56429c540a998a68ab55
 echo "This line is added from develope branch";
 
 ?>
